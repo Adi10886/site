6a01ba23ee228f246e1675c16a19fee492b34222
Adi10886 <77274787+Adi10886@users.noreply.github.com>
2022-03-30 21:30:19 +0530

    Update contact.js

--- contact.js
+++ contact.js
@@ -7,3 +7,15 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+
+/*chat-box*/
+
+  
+document .querySelector(".openChatBtn") .addEventListener("click", openForm);
+document.querySelector(".close").addEventListener("click", closeForm);
+function openForm() {
+   document.querySelector(".openChat").style.display = "block";
+}
+function closeForm() {
+   document.querySelector(".openChat").style.display = "none";
+}
